3a39ff3a7395d4a5bef0ba1094db3cdb73c1e005
davidcoats <david@getsafetycone.com>
2023-06-19 14:26:32 -0700

    Initial commit.

---
+++ source/R5T.T0210/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0210
+{
+	/// <summary>
+	/// Project file XML text type (ProjectFileXmlText).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0210/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0210
+Project file XML text type (ProjectFileXmlText).
